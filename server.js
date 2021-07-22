dc5da22bf2f97812d9cb5e067d0a5748f2c6b0fe
Michael Krumland-Dunning <axolotlman2@gmail.com>
2021-07-22 00:28:33 -0700

    wd

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('port', process.env.PORT || 9501);
 
-app.get('/api', (req, res) => res.json('Hello World!'))
+app.get('/api', (req, res) => res.json(
+  {
+      name: 'Michael',
+      age: 15
+))
 
 var server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
