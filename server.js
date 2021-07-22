30e381ef7d4f5ce49fdbb7a4c52a804dee42dbe4
Michael Krumland-Dunning <axolotlman2@gmail.com>
2021-07-22 00:25:05 -0700

    wd

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || 9501);
+
+app.get('/', (req, res) => res.json('Hello World!'))
 
 var server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
